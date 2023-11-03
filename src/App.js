7c0d9e29921a8a37dff9262748097bcdaa556e07
Chris Ho <cho@nerdwallet.com>
2023-11-03 08:04:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to learn React!
         </a>
       </header>
     </div>
